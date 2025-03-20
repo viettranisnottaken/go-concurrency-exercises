9a54750292895c77e66ff9dca7162dc084ec7bdd
Viet Tran <tranxuanviet1997@gmail.com>
2025-03-20 22:09:20 +0700

    Limit service time

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,11 @@
 
 package main
 
+import (
+	"context"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,7 +26,25 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
+	ctx := context.Background()
+	withTimeout, _ := context.WithTimeout(ctx, time.Second*10)
+	done := make(chan struct{})
+
+	go func() {
+		process()
+		done <- struct{}{}
+	}()
+
+	if !u.IsPremium {
+		select {
+		case <-done:
+			return false
+		case <-withTimeout.Done():
+			return false
+		}
+	}
+
+	<-done
 	return true
 }
 
--- 3-limit-service-time/mockserver.go
+++ 3-limit-service-time/mockserver.go
@@ -39,12 +39,14 @@ func RunMockServer() {
 
 func createMockRequest(pid int, fn func(), u *User) {
 	fmt.Println("UserID:", u.ID, "\tProcess", pid, "started.")
+	start := time.Now()
 	res := HandleRequest(fn, u)
+	duration := time.Since(start).Seconds()
 
 	if res {
-		fmt.Println("UserID:", u.ID, "\tProcess", pid, "done.")
+		fmt.Println("UserID:", u.ID, "\tProcess", pid, "done in", duration)
 	} else {
-		fmt.Println("UserID:", u.ID, "\tProcess", pid, "killed. (No quota left)")
+		fmt.Println("UserID:", u.ID, "\tProcess", pid, "killed. (No quota left), in", duration)
 	}
 
 	wg.Done()
